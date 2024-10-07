af2f644ea76b97bdbab929b00ef65ee64d14ba8f
Buddhika <buddhikadilupulr@gmail.com>
2024-10-07 11:57:29 +0530

    swagger updated

--- swaggerGen.ts
+++ swaggerGen.ts
@@ -1,12 +1,14 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+const isLocal = false; // Check if the environment is local
+
 const doc = {
   info: {
     title: 'API Documentation',
     description: 'API documentation for the project',
   },
-  host: 'localhost:4040', // Update this with your actual host/port
-  schemes: ['http'],
+  host: isLocal ? 'localhost:4040' : 'us-central1-pos-kandy.cloudfunctions.net/kandy-posapplication',
+  schemes: [isLocal ? 'http' : 'https'],
 };
 
 const outputFile = './swagger-output.json';
